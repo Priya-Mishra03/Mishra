28fec3193dc58fffcb9a39e2b8c6b1e87e4ced0f
Priya-Mishra03 <145385545+Priya-Mishra03@users.noreply.github.com>
2023-10-01 21:01:01 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 00:30:12 2023
+
+@author: DKM
+"""
+
+import turtle 
+t = turtle . Turtle ()
+turtle . forward (80)
+turtle. right (144)
+turtle. forward (80)
+turtle . right (144)
+turtle . forward (80)
+turtle . right (144)
+turtle. forward (80)
+turtle . right (144)
+turtle . forward (80)
