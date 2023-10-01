598d535a9d7922d8e8822f6d12580ae216c6ae0c
Priya-Mishra03 <145385545+Priya-Mishra03@users.noreply.github.com>
2023-10-01 21:00:09 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,13 @@
+import turtle
+t = turtle. Turtle ()
+turtle . forward (150)
+turtle . left (90)
+turtle . forward (150)
+turtle . left (90)
+turtle . forward (150)
+turtle . left (90)
+turtle . forward (150)
+turtle . left (90)
+
+
+
