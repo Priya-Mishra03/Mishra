19ad4db09a53d7aa2e279d5fa30edcb3ce12c306
Priya-Mishra03 <145385545+Priya-Mishra03@users.noreply.github.com>
2023-10-01 21:01:38 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 00:03:32 2023
+
+@author: DKM
+"""
+
+import turtle 
+t = turtle. Turtle ()
+turtle . left (120)
+turtle . forward (150)
+turtle . left (120)
+turtle . forward (150)
+turtle . left (120)
+turtle . forward (150)
